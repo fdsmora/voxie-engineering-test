9011ce187b64bec6f2694cf03dd189d7cab54db5
Fausto <fausto@fausto.com>
2020-08-26 11:19:56 -0500

    view teams client test

---
+++ tests/test_view.py
@@ -0,0 +1,18 @@
+import pytest
+from project.db import get_db
+
+def test_view(client, app):
+    test_team_names = ['Fausto','David','Tomas']
+    
+    with app.app_context():
+        db = get_db()
+        for name in test_team_names:
+            db.execute(
+                "INSERT INTO TEAMS (name) VALUES (?)", (name,)
+            )
+        db.commit()
+
+    response = client.get('/view')
+    assert response.status_code == 200
+    for name in test_team_names:
+        assert bytes(name) in response.data
