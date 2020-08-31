8db3504173c8e154db6e157cdcd4bdf1e0c242b9
fdsmora <fausto@fausto.com>
2020-08-31 08:52:27 -0500

    Improved UX

--- project/static/actions.js
+++ project/static/actions.js
@@ -1,11 +1,11 @@
 $(document).ready(
   function(){
-    $("form a").click(function(){ 
-        if ($(this).text()=="Edit"){
+    $("form input").click(function(){ 
+        if ($(this).val()=="Edit"){
             var closestParentRow = $(this).parent().closest('div').eq(0);
             closestParentRow.find(".viewmode").hide();
             closestParentRow.find(".editmode").show();
-        }else if ($(this).text()=="Cancel"){
+        }else if ($(this).val()=="Cancel"){
             var closestParentRow = $(this).parent().closest('div').eq(0);
             closestParentRow.find(".viewmode").show();
             closestParentRow.find(".editmode").hide();
